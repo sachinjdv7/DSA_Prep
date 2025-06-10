df19d0a1e98bd63fbc10fff08df96bb1b4b5464e
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-10 14:50:07 +0530

    learn two more number pattern

--- pattern/q2.js
+++ pattern/q2.js
@@ -15,3 +15,39 @@ for (let i = 1; i <= 5; i++) {
     }
     console.log()
 }
+
+/* 
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+*/
+
+console.log("....number pattern1 ......")
+
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write(j.toString() + " ")
+    }
+    console.log()
+}
+
+/* 
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+
+*/
+console.log("........pattern 3 ..............")
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        // if (i === j)
+        process.stdout.write(i.toString() + " ")
+    }
+    console.log()
+}
