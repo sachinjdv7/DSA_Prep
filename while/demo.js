bb78ec21dbf316883534cb1bf95f1f5d96bb0865
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-13 20:35:29 +0530

    while explanation

--- while/demo.js
+++ while/demo.js
@@ -5,6 +5,9 @@
  // code 
   change
  }
+  --> jab tak number na mil jaye tab tak apn ko chalna hai.
+  --> jab tak 5000 nahi khatam hote tab tak shopping karni hai
+  --> ya bas hame kad rukna bas pata hai
 */
 
 let i = 1;
