1ed8acf8d88aeb30179ac50b2befdeb0d044756f
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-13 17:28:03 +0530

    more optimize

--- for_loop/prime.js
+++ for_loop/prime.js
@@ -4,7 +4,7 @@ function isPrime(num) {
     if (!Number.isInteger(num) || isNaN(num) || num === undefined || num <= 1) {
         return false;
     }
-
+    // make iteration half
     for (let i = 2; i <= Math.sqrt(num); i++) {
         if (num % i === 0) {
             return false
@@ -15,3 +15,21 @@ function isPrime(num) {
 // Example of user input (in a real-world scenario, this could be from a form or console input)
 const userInput = 5;
 console.log(`Is ${userInput} a prime number? `, isPrime(userInput));
+
+
+// more optimize solution
+
+function isPrimePro(n) {
+    if (n <= 1) return false;
+    if (n === 2) return true
+    if (n % 2 === 0) return false
+
+    for (let i = 3; i <= Math.floor(Math.sqrt(n)); n += 2) {
+        if (n % i) return false
+
+    }
+
+    return true
+}
+
+console.log(isPrimePro(6546546546512))
