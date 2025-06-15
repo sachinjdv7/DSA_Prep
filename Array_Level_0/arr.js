fa26ac96869c315b737a46f7193a8c8f4db19bb4
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-15 16:43:53 +0530

    demo array

---
+++ Array_Level_0/arr.js
@@ -0,0 +1,35 @@
+const arr = [10, 20, 30, 40];
+// crud operation
+
+arr.push(50);
+arr.push(60);
+
+console.log(arr.pop())
+
+// as a best practice adding element using index
+
+const arr1 = [];
+
+arr1[0] = 20
+arr1[1] = 40
+arr1[2] = 60
+arr1[3] = 80
+arr[6] = 100
+
+console.log(arr1)
+
+// using loop add values into the array
+
+const arr3 = []
+
+for (let i = 0; i < 5; i++) {
+    // arr3.push(i)
+    arr3[i] = i * 10
+}
+
+arr3
+
+
+
+
+
