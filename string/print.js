ef43b6870fc42c75b6d683a60a230b7a2ff3c5cb
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-16 21:04:12 +0530

    str print rev and straight

---
+++ string/print.js
@@ -0,0 +1,12 @@
+
+let a = "cool"
+
+for (let i = 0; i < a.length; i++) {
+    // console.log(a[i])
+    console.log(a.charAt(i))
+}
+
+// reverse
+for (let i = a.length - 1; i >= 0; i--) {
+    console.log(a.charAt(i))
+}
