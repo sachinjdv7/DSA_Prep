096b36eef79d707b1efe58b18dfb823565af32e4
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-18 00:25:34 +0530

    o(1) space vowels lookup

--- string/vowel_count_str.js
+++ string/vowel_count_str.js
@@ -15,3 +15,48 @@ function vowelCount(str) {
 }
 
 console.log(vowelCount(str))
+
+/* 
+time complexity - O(n)
+space complexity - O(n)
+*/
+//========================================
+
+function vowelCount1(str) {
+
+    let vowels = new Set(['a', 'e', 'i', 'o', 'u'])
+    // return vowels
+    let count = 0
+    for (char of str) {
+        vowels.has(char.toLowerCase()) && count++
+    }
+    // for (let i = 0; i < str.length; i++) {
+    //     let char = str.charAt(i).toLowerCase()
+    //     if (vowels.has(char)) {
+    //         count++
+    //     }
+    // }
+    return count
+
+}
+console.log(vowelCount1("hey how are you"))
+
+/* 
+time complexity - O(n)
+space complexity - O(1)
+*/
+
+
+
+
+const s = new Set();
+s.add(1);
+s.add(2);
+s.add(2); // ignored
+console.log(s); // Set(2) { 1, 2 }
+
+
+let nums = [1, 2, 2, 3, 4, 4];
+let unique = [...new Set(nums)];
+console.log(unique); // [1, 2, 3, 4]
+
