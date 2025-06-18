13a291822340e5d881e937cf0b13cea9b832b557
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-18 17:55:32 +0530

    anoter approach frequency

--- string/frequency_str.js
+++ string/frequency_str.js
@@ -19,3 +19,38 @@ function frequency(str) {
 
 }
 console.log(frequency("hello"))
+
+
+function frequencyMap(str) {
+    let map = new Map()
+    let res = []
+
+    for (let char of str) {
+
+        if (!map.has(char)) {
+            map.set(char, 1)
+        } else {
+            map.set(char, map.get(char) + 1)
+        }
+    }
+
+    for (let [char, count] of map) {
+        res.push(`${char} appears ${count} time${count > 1 ? "s" : ""}`)
+    }
+
+    return res.join("\n")
+}
+console.log(frequencyMap("hello"))
+
+
+let map = new Map();
+map.set("h", 1);
+map.set("e", 1);
+map.set("l", 2);
+map.set("o", 1);
+
+console.log(map)
+
+for (char of map) {
+    console.log(char)
+}
