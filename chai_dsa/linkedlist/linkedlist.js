23ae028af96f70391b8727a2384fe54cfead34af
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-19 18:30:01 +0530

    linked list add at beginning and end

---
+++ chai_dsa/linkedlist/linkedlist.js
@@ -0,0 +1,32 @@
+
+class Node {
+    constructor(data, next = null) {
+        this.data = data
+        this.next = next
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+}
+
+
+LinkedList.prototype.insertAtBeginning = function (data) {
+    let newNode = new Node(data)
+    this.head = newNode
+}
+
+LinkedList.prototype.insertAtEnd = function (data) {
+    const newNode = new Node(data)
+    if (!this.head) {
+        this.head = newNode
+    }
+    let last = this.head
+    while (last.next) {
+        last = last.next
+    }
+
+    last.next = newNode
+}
