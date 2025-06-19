328db173c89ef9e0dda406ab50f85d8af94f8e3b
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-19 15:01:13 +0530

    learn queue

---
+++ chai_dsa/queue/queue.js
@@ -0,0 +1,37 @@
+class Queue {
+    constructor() {
+        this.queue = []
+    }
+
+    enqueue(data) {
+        this.queue.push(data)
+    }
+    dequeue() {
+        if (this.isEmpty()) {
+            return "Queue is empty"
+        }
+        return this.queue.shift()
+    }
+    isEmpty() {
+        return this.queue.length === 0
+    }
+    peek() {
+        if (this.isEmpty()) {
+            return "Queue is empty"
+        }
+        return this.queue[0]
+    }
+    size() {
+        return this.queue.length
+    }
+    clear() {
+        this.queue = []
+    }
+    printQueue() {
+        let str = ""
+        for (let i = 0; i < this.queue.length; i++) {
+            str += this.queue[i] + "\n"
+        }
+        return str
+    }
+}
