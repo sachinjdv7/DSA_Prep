8ed7b8d60204dfb5ddfd24ba83bf7b538266c208
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-21 17:37:29 +0530

    testing understanding

---
+++ list.js
@@ -0,0 +1,117 @@
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next
+    }
+}
+
+class List {
+    constructor() {
+        this.head = null
+    }
+
+    insertAtBeginning(data) {
+        let newNode = new Node(data, this.head)
+        this.head = newNode
+    }
+
+    insertAtLast(data) {
+        let newNode = new Node(data)
+        if (!this.head) {
+            this.head = newNode
+            return
+        }
+        let last = this.head
+
+        while (last.next) {
+            last = last.next
+        }
+
+        last.next = newNode
+    }
+
+    insertAtGivenNode(prevNode, data) {
+        if (!prevNode) "Previous node can not be null"
+        let newNode = new Node(data, prevNode.next)
+        prevNode.next = newNode
+    }
+
+    deleteHead() {
+        if (!this.head) return "empty list nothing to delete"
+
+        this.head = this.head.next
+    }
+
+    deleteLast() {
+        // empty head
+        if (!this.head) return "nothing to delete"
+        // only one value
+        if (!this.head.next) {
+            this.head = null
+            return
+        }
+
+        let secondLast = this.head
+
+        while (secondLast.next.next) {
+            secondLast = secondLast.next
+        }
+
+        secondLast.next = null
+
+    }
+
+    // delete by key
+
+    deleteByKey(key) {
+        if (!this.head) return "nothing to delete";
+
+        if (this.head.data === key) {
+            this.head = this.head.next;
+            return;
+        }
+
+        let current = this.head;
+
+        while (current.next) {
+            if (current.next.data === key) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+
+        return `No node found with key ${key}`;
+    }
+
+
+    // traversal
+    printList() {
+        let current = this.head
+        if (!current) return "List is empty"
+        let arr = []
+        while (current) {
+            arr.push(current.data)
+            current = current.next
+        }
+        return arr.join(" -> ")
+    }
+
+}
+
+const list = new List()
+list.insertAtBeginning(30)
+// list.insertAtBeginning(40)
+
+list.insertAtLast(10)
+list.insertAtLast(20)
+// list.deleteHead()
+// list.deleteHead()
+// list.deleteLast()
+// list.deleteLast()
+// list.deleteLast()
+// list.deleteByKey(30)
+list.deleteByKey(20)
+
+console.log(list.printList())
+
