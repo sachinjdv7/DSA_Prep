2e16a3ceb5d45ed11637c7ef91788029219fbb84
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-23 21:34:17 +0530

    insert node in bst

--- chai_dsa/BST/bst.js
+++ chai_dsa/BST/bst.js
@@ -10,4 +10,35 @@ class BinarySearchTree {
     constructor() {
         this.root = null
     }
+
+    insert(key) {
+
+        const newNode = new BSTNode(key)
+
+        if (this.root === null) {
+            this.root = newNode
+        } else {
+            this.insertNode(this.root, newNode)
+        }
+    }
+
+    insertNode(node, newNode) {   // node is root
+        if (newNode.key < node.key) {
+
+            if (node.left === null) {
+                node.left = newNode
+            } else {
+                this.insertNode(node.left, newNode)
+            }
+        } else {
+            if (newNode.key > node.key) {
+
+                if (node.right === null) {
+                    node.right = newNode
+                } else {
+                    this.insertNode(node.right, newNode)
+                }
+            }
+        }
+    }
 }
