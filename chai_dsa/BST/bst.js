9a5182036af75c8b4cc72cd2b750d1f16d693798
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-23 22:14:17 +0530

    refactor if else with return

--- chai_dsa/BST/bst.js
+++ chai_dsa/BST/bst.js
@@ -17,28 +17,31 @@ class BinarySearchTree {
 
         if (this.root === null) {
             this.root = newNode
-        } else {
-            this.insertNode(this.root, newNode)
+            return
         }
+
+        return this.insertNode(this.root, newNode)
+
     }
 
-    insertNode(node, newNode) {   // node is root
+    insertNode(node, newNode) {
         if (newNode.key < node.key) {
-
             if (node.left === null) {
-                node.left = newNode
-            } else {
-                this.insertNode(node.left, newNode)
+                node.left = newNode;
+                return;
             }
-        } else {
-            if (newNode.key > node.key) {
-
-                if (node.right === null) {
-                    node.right = newNode
-                } else {
-                    this.insertNode(node.right, newNode)
-                }
-            }
+            return this.insertNode(node.left, newNode);
         }
+
+        if (newNode.key > node.key) {
+            if (node.right === null) {
+                node.right = newNode;
+                return;
+            }
+            return this.insertNode(node.right, newNode);
+        }
+
+        // Duplicate key: do nothing
     }
+
 }
