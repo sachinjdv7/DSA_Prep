86508d17830fd957c146d2322817fc512cc1993c
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-23 13:22:13 +0530

    BST implementation

---
+++ chai_dsa/BST/bst.js
@@ -0,0 +1,13 @@
+class BSTNode {
+    constructor(key) {
+        this.key = key;
+        this.left = null;
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+}
