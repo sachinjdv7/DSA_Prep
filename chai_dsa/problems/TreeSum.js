738710008dfa7af37d4405e6b515567d55f0d822
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-24 17:30:00 +0530

    treesum recursion

--- chai_dsa/problems/TreeSum.js
+++ chai_dsa/problems/TreeSum.js
@@ -16,3 +16,30 @@ const BFTSum = (root) => {
     }
     return sum
 }
+
+const DFSSum = (root) => {
+
+    if (root === null) return 0
+
+    const stack = [root]
+    let sum = 0
+
+    while (stack.length > 0) {
+
+        const node = stack.pop()
+        sum += node.key
+
+        if (node.left) stack.push(node.left)
+        if (node.right) stack.push(node.right)
+    }
+    return sum
+}
+
+// recursive
+
+const recursive = (root) => {
+
+    if (root === null) return 0
+
+    return root.key + recursive(root.left) + recursive(root.right)
+}
