6049aab969e981958c9ee8e862042ecadc018176
Sachin Jadhav <sachinjdv7@gmail.com>
2025-06-26 12:54:02 +0530

    recursive count of all occurence

--- chai_dsa/recursion/count_all_occurence.js
+++ chai_dsa/recursion/count_all_occurence.js
@@ -13,3 +13,13 @@ function countKeyIA(arr, key) {
     return count
 }
 console.log(countKeyIA([1, 2, 3, 4, 2, 8, 7, 2], 2))
+
+function recursiveCountKeyIA(arr, key, index) {
+
+    if (index >= arr.length) return 0
+
+    return (arr[index] === key ? 1 : 0) + recursiveCountKeyIA(arr, key, index + 1)
+
+}
+
+console.log(recursiveCountKeyIA([1, 2, 3, 4, 2, 8, 7, 2], 2, 0))
