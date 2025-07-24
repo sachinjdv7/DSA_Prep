e78f7fbcf57b98b2b65e20e65c84f38ee73317a0
Sachin Jadhav <sachinjdv7@gmail.com>
2025-07-24 13:11:09 +0530

    twoSum using map

--- hashing table/twoSum.js
+++ hashing table/twoSum.js
@@ -19,3 +19,27 @@ console.log(twoSum([15, 2, 11, 7], 9))
  * time complexity: O(n^2)
  * space complexity: O(1)
  */
+
+// second approach
+
+function twoSum1(arr, target) {
+
+    let map = new Map()
+    for (let i = 0; i < arr.length; i++) {
+
+
+        if (map.has(target - arr[i])) {
+            return [i, map.get(target - arr[i])]
+        } else {
+            map.set(arr[i], i)
+        }
+    }
+    return [-1, -1]
+}
+
+console.log(twoSum1([15, 2, 11, 7], 9))
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(n)
+ */
