3b237fc36cb4be8079ad35c83e8f7df8db0e1e73
Sachin Jadhav <sachinjdv7@gmail.com>
2025-08-02 15:12:30 +0530

    toggle theme using js

--- DOM/01_dom/index.js
+++ DOM/01_dom/index.js
@@ -1,3 +1,16 @@
-function changeBackground(color) {
-    document.body.style.backgroundColor = color
-}
+
+const themeButton = document.getElementById('theme-btn')
+
+
+themeButton.addEventListener('click', () => {
+
+    const currentColor = document.body.style.backgroundColor;
+
+    if (!currentColor || currentColor === 'white') {
+        document.body.style.backgroundColor = "black"
+        themeButton.innerHTML = "white"
+    } else {
+        document.body.style.backgroundColor = "white"
+        themeButton.innerHTML = "black"
+    }
+})
